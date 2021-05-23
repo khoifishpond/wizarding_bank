9fce2956fb9f9e3bd290daf981a702fc7bd619dc
Khoi Nguyen <khoinguyen311@gmail.com>
2021-05-22 23:57:23 -0700

    Add open_credit method

--- lib/credit_card.rb
+++ lib/credit_card.rb
@@ -1,7 +1,13 @@
 class CreditCard
-  attr_reader :bank
+  attr_reader :bank, :person
 
   def initialize(bank)
     @bank = bank
+  end
+
+  def open_credit(person, limit, interest)
+    @person = person
+    @limit = limit
+    @interest = interest
   end
 end
