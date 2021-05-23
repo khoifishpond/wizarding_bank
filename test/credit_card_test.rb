9f7201cb2ebe93cd14c7bf99409c3ddd8d35c576
Khoi Nguyen <khoinguyen311@gmail.com>
2021-05-22 23:59:33 -0700

    Add belongs to test

--- test/credit_card_test.rb
+++ test/credit_card_test.rb
@@ -1,11 +1,21 @@
 require 'minitest/autorun'
 require 'minitest/pride'
 require './lib/credit_card'
+require './lib/person'
 
 class CreditCardTest < Minitest::Test
   def test_credit_card_exists
     amex = CreditCard.new("AMEX")
 
     assert_instance_of(CreditCard, amex)
+  end
+
+  def test_credit_card_belongs_to_a_person
+    amex = CreditCard.new("AMEX")
+    person = Person.new("Minerva", 1000)
+
+    amex.open_credit(person, 100, 0.5)
+
+    assert_equal("Minerva", amex.person.name)
   end
 end
